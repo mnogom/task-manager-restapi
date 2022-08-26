27e8676a8bf1008bfac778a057e3744f98bd7aae
Konstantin Freidlin <freidlinks@ya.ru>
2022-08-26 16:11:13 +0300

    Update task serializer, remove auth protection

--- task_manager/apps/task/serializers.py
+++ task_manager/apps/task/serializers.py
@@ -1,10 +1,14 @@
 """Serializers."""
 
 from rest_framework import serializers
+
+from task_manager.apps.label.serializers import LabelSerializer
 from .models import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    labels = LabelSerializer(many=True)
+
     class Meta:
         model = Task
         fields = (
--- task_manager/apps/task/views.py
+++ task_manager/apps/task/views.py
@@ -9,13 +9,13 @@ from .serializers import TaskSerializer
 
 
 class ListCreateTaskView(ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
+    # permission_classes = (IsAuthenticated,)
     queryset = Task.objects.order_by('created_at')
     serializer_class = TaskSerializer
 
 
 class RetrieveUpdateDestroyTaskView(RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)
+    # permission_classes = (IsAuthenticated,)
     http_method_names = ('get', 'patch', 'delete', 'head', 'options')
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
