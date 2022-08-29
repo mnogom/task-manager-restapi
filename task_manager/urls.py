bbabb1b10e31a0f392ed97dd140ac6876641c115
Konstantin Freidlin <freidlinks@ya.ru>
2022-08-29 11:51:26 +0300

    Minor dev optimisation

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -4,6 +4,8 @@ https://djoser.readthedocs.io/en/latest/authentication_backends.html
 Djoser provides this urls:
     path('auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
+
+Djoser endpoints: https://djoser.readthedocs.io/en/latest/base_endpoints.html
 """
 
 from django.contrib import admin
